7cbd54bec7119abf23010a1a4572960d40204023
Hoa-Cao <thi.cao15@students.vu.edu.au>
2021-10-20 20:05:01 +1100

    The second data

---
+++ Banking Data.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(rtweet)
+
+#every week do this (19 Oct)
+data<-search_tweets(
+  q="baking",
+  n=18000,
+  include_rts = FALSE,
+  lang="en",
+  retryonrtelimit = TRUE
+)
+
+data<-data%>%flatten()
+
+#change the name of the file
+#if you dont, you are going to lose your data!!!
+
+data%>%write_csv("16_10_2021.csv")
+
