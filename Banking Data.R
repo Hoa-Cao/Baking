5e76eddb3e942bc94dbc6860ab1bde61f8cdca93
Hoa-Cao <thi.cao15@students.vu.edu.au>
2021-11-06 07:51:23 +1100

    test

--- Banking Data.R
+++ Banking Data.R
@@ -15,5 +15,12 @@ data<-data%>%flatten()
 #change the name of the file
 #if you dont, you are going to lose your data!!!
 
-data%>%write_csv("16_10_2021.csv")
+data%>%write_csv("21_10_2021.csv")
 
+data<-search_tweets(
+  q="baking",
+  n=18000,
+  include_rts = FALSE,
+  lang="en",
+  retryonrtelimit = TRUE
+)
